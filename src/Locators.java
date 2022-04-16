d898c309a53d8a91dcc6b72ce8e8648cd4dd8c8e
Qusai Mousa <qusaimousa@qusais-air.mynet>
2022-04-17 00:47:14 +0300

    save work

--- src/Locators.java
+++ src/Locators.java
@@ -1,6 +1,8 @@
 
-
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import org.openqa.selenium.By;
@@ -9,107 +11,99 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import constatns.LoginTestsConstants;
 import config.properties.Properties;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 public class Locators {
 
     private static String username = "Qusai@gmail.com";
     private static String password = "123456789";
 
     public static WebDriver driver;
-@AfterTest
-public void closeTab(){
-    driver = new ChromeDriver();
-    driver.close();
 
+    @BeforeTest
+    public void launchBrowser() {
+        System.setProperty("webdriver.chrome.driver", Properties.DRIVER_PATH);
+        driver = new ChromeDriver();
+    }
 
-}
+    @AfterTest
+    public void CloseBrowser() {
+        driver.close();
+    }
+
 
     public static void main(String[] args) {
     }
 
     @Test
     public static void TestHomeButtton() {
-
-
-        System.setProperty("webdriver.chrome.driver", Properties.DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://a.testaddressbook.com");
+        driver.navigate().to(Properties.BROWSER_LINK);
         driver.findElement(By.className("nav-item")).click();
+        String actualResult = driver.getTitle();
+        String exeptedResult = "Address Book";
+        assertEquals(exeptedResult, actualResult);
+
     }
 
 
     @Test
     public static void TestSignInPage() {
-
-        System.setProperty("webdriver.chrome.driver", Properties.DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://a.testaddressbook.com");
+        driver.navigate().to(Properties.SIGNIN_LINK);
         driver.findElement(By.id("sign-in")).click();
+        String actualResult = driver.getTitle();
+        String exeptedResult = "Address Book - Sign In";
+        assertEquals(exeptedResult, actualResult);
+
 
     }
 
     @Test
     public static void EmailAndPasswordEmpty() {
-
-        System.setProperty("webdriver.chrome.driver", Properties.DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://a.testaddressbook.com/sign_in");
+        driver.navigate().to(Properties.SIGNIN_LINK);
         driver.findElement(By.cssSelector("input.form-control")).sendKeys("");
         driver.findElement(By.id(LoginTestsConstants.SESSSION_PASSWORD)).sendKeys("");
         driver.findElement(By.className(LoginTestsConstants.BTN_PRIMARY)).click();
+       String notice = driver.findElement(By.className("alert")).getText();
+        String actualResult = notice;
+        String exeptedResult = "Bad email orpassword.";
+        Assert.assertNotEquals(exeptedResult, actualResult);
     }
 
     @Test
     public static void FillEmailAndPassword() {
 
-        System.setProperty("webdriver.chrome.driver", Properties.DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://a.testaddressbook.com/sign_in");
+        driver.navigate().to(Properties.SIGNIN_LINK);
         driver.findElement(By.className("form-control")).sendKeys(username);
         driver.findElement(By.id(LoginTestsConstants.SESSSION_PASSWORD)).sendKeys(password);
         driver.findElement(By.className(LoginTestsConstants.BTN_PRIMARY)).click();
+        String notice = driver.findElement(By.className("alert")).getText();
+        Boolean verifyResult = notice.equalsIgnoreCase("Bad email or password.");
+        AssertJUnit.assertNotNull(verifyResult);
     }
 
     @Test
     public static void FillEmailAndPasswordEmpty() {
 
-        System.setProperty("webdriver.chrome.driver", Properties.DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://a.testaddressbook.com/sign_in");
+
+        driver.navigate().to(Properties.SIGNIN_LINK);
         driver.findElement(By.className("form-control")).sendKeys(username);
         driver.findElement(By.cssSelector("input#session_password")).sendKeys("");
         driver.findElement(By.className(LoginTestsConstants.BTN_PRIMARY)).click();
+        String notice = driver.findElement(By.className("alert")).getText();
+        Boolean verifyResult = notice.equalsIgnoreCase("Bad email or password..");
+        Assert.assertFalse(verifyResult);
     }
 
     @Test
     public static void FillPasswordAndEmailEmpty() {
 
-        System.setProperty("webdriver.chrome.driver", Properties.DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://a.testaddressbook.com/sign_in");
+        driver.navigate().to(Properties.SIGNIN_LINK);
         driver.findElement(By.xpath("//input[@class='form-control']")).sendKeys("");
         driver.findElement(By.id(LoginTestsConstants.SESSSION_PASSWORD)).sendKeys(password);
         driver.findElement(By.className(LoginTestsConstants.BTN_PRIMARY)).click();
-
-    }
-
-    @Test
-    public static void testAssertTitle() {
-        System.setProperty("webdriver.chrome.driver", Properties.DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://a.testaddressbook.com");
-        String ActualTitle = driver.getTitle();
-        String ExeptedTitle = "Address Book";
-        AssertJUnit.assertEquals(ExeptedTitle, ActualTitle);
-    }
-
-    @Test
-    public static void testSignInIsClicked() {
-        System.setProperty("webdriver.chrome.driver", Properties.DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://a.testaddressbook.com");
-        driver.findElement(By.id("sign-in")).click();
-        AssertJUnit.assertFalse(driver.findElement(By.id("sign-in")).isSelected());
-
+        String notice = driver.findElement(By.className("alert")).getText();
+        Boolean verifyResult = notice.equalsIgnoreCase("Bad email or password.");
+        Assert.assertTrue(verifyResult);
 
     }
 
--- src/config/properties/Properties.java
+++ src/config/properties/Properties.java
@@ -2,5 +2,8 @@ package config.properties;
 
 public class Properties {
     public static  String DRIVER_PATH = "/Users/qusaimousa/downloads/chromedriver";
+    public static String BROWSER_LINK = "http://a.testaddressbook.com";
+    public static String SIGNIN_LINK = "http://a.testaddressbook.com/sign_in";
+
 
 }
